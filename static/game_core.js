2a8223dafdc88f2917d0c01378884ee5741fbdf7
Pranav Patil <4pranavpatil@gmail.com>
2020-06-18 15:47:37 -0700

    Improved documentation

--- static/game_core.js
+++ static/game_core.js
@@ -15,14 +15,18 @@ var game_core = function(game_instance){
         height : 480
     };
 
+    // representation of players received from server (stored as client_player objects)
     this.players = [];
 
+    // client_player that uses client prediction
     this.client;
 
+    // a queue of all movements yet to be handled by server. once server has handled movement, it is dequeued
     this.movementQueue = [];
+    // number used to identify order of movements, which to remove
     this.movementNum = 0;
 
-    // stores which keys are pressed (either WASD or arrow keys)
+    // stores which keys are pressed
     this.movement = {
         up: false,
         down: false,
@@ -30,36 +34,60 @@ var game_core = function(game_instance){
         right: false
     }
     
+    // loads spritesheets
     this.load_images();
+
+    // adds listeners on keyup and keydown
     this.setup_keybinds();
+
+    // connects to server, requests new player
     this.connect_to_server();
+
+    // fast browser update (faster than 60 fps???)
     window.requestAnimationFrame(this.browser_update.bind(this));
+
+    // sending movementQueue to server (500ms wait to simulate network latency)
     setInterval(this.short_update.bind(this), 500);
+
+    // on state received back, update players list, blocks, handle movement queue
     this.socket.on('state', this.update_state.bind(this));
 
 }; // game_core.constructor
 
+/**
+ * handles data received from server. updates players list (creates new if needed)
+ */
 game_core.prototype.update_state = function(gameState) {
+    var playersExist = {};
     // take each of the dicts returned and do this
     for (var id in gameState.players) {
+        playersExist[id] = true;
         if (this.players[id] === undefined) {
             this.players[id] = new client_player(gameState.players[id]);
         } else {
             this.players[id].update_vals(gameState.players[id]);
         }
     }
+    // checks if player is gone, then removes them from local list
+    for (var id in this.players) {
+        if (!playersExist[id])
+            delete this.players[id];
+    }
+    // updates blocks (don't change now but may later)
     this.blocks = gameState.blocks;
 
+    // removes any elements in movement queue that were already processed by server
+    // -1 flag means no movements received, ever
     if (this.movementQueue.length > 0 && gameState.players[this.socket.id] !== undefined && 
             gameState.players[this.socket.id].lastNum !== -1) {
         var first = this.movementQueue[0].num;
         this.movementQueue.splice(0, gameState.players[this.socket.id].lastNum - first + 1);
-        // console.log("START:", first);
-        // console.log("NEW:", (this.movementQueue[0] || {}).num);
     }
-    // console.log('update state');
 };
 
+/**
+ * adds listeners to key up and down to handle movement on both arrow keys and WASD
+ */
 game_core.prototype.setup_keybinds = function() {
     document.addEventListener('keydown', function(event) {
         switch (event.keyCode) {
@@ -121,6 +149,7 @@ game_core.num_to_mvmt = function (num) {
 }
 
 game_core.prototype.browser_update = function() {
+    // adds movements. stores order, time, and number representing movement requested
     this.movementQueue.push(
         {
             num: this.movementNum,
@@ -130,25 +159,36 @@ game_core.prototype.browser_update = function() {
     );
     this.movementNum ++;
 
+    // client prediction
     this.simulate_physics();
+
+    // draws everything
     this.draw_world();
+
+    // loads function again (creates a loop)
     requestAnimationFrame(this.browser_update.bind(this));
 };
 
+/**
+ * simulate_physics method has the same physics code as server but operates on movements
+ * not handled by server yet. uses server's knowledge of player position as a baseline and moves
+ * from there
+ */
 game_core.prototype.simulate_physics = function() {
+    // need a player and movements to simulate
     if (this.client == undefined) return;
     if (this.players[this.socket.id] == undefined) return;
     if (this.movementQueue.length == 0) return;
 
+    // set player to simulate to the client variable
     var player = this.client;
+    // update values
+    // (important: don't do it directly like this.client.update_vals or the definition of this gets confused)
     player.update_vals(this.players[this.socket.id]);
 
     var speed = 200;
 
-    var lastNum = player.lastNum;
-
-    // console.log(this.movementQueue[this.movementQueue.length - 1].t);
-
+    // loops through all movements (queue only has ones not handled by server)
     for (var j in this.movementQueue) {
         var movement = this.movementQueue[j];
         var data = game_core.num_to_mvmt(movement.mvmt);
@@ -209,9 +249,15 @@ game_core.prototype.simulate_physics = function() {
 game_core.prototype.short_update = function() {
     if (document.hasFocus()) {
         this.socket.emit('movement', this.movementQueue);
+    } else {
+        // todo: need some pause functionality. server stops simulating player
+        // should the player continue moving with last request if server lags more than 1 second or just stuck?
     }
 };
 
+/**
+ * uses the context and draws all the blocks and players
+ */
 game_core.prototype.draw_world = function () {
     this.ctx.clearRect(0, 0, this.world.width, this.world.height);
 
@@ -227,9 +273,9 @@ game_core.prototype.draw_world = function () {
         var player = this.players[id];
         if (id == this.socket.id) {
             this.client.draw(this, this.ctx);
+        } else {
+            player.draw(this, this.ctx);
         }
-           player.draw(this, this.ctx);
-        
     }
 
     // assumes a constant y value for now, places a 10px tall rectangle for blocks
@@ -240,12 +286,18 @@ game_core.prototype.draw_world = function () {
     }
 };
 
+/**
+ * request new player. est. socket connection. store local client for prediction
+ */
 game_core.prototype.connect_to_server = function() {
     this.socket = io();
     this.socket.emit('new player');
     this.client = new client_player();
 };
 
+/**
+ * create dictionary of spritesheets
+ */
 game_core.prototype.load_images = function() {
     this.images = {};
     this.images.stand = new Image();
@@ -254,6 +306,9 @@ game_core.prototype.load_images = function() {
     this.images.walk.src = '/static/stickman_walk.png';
 };
 
+/**
+ * defines a client_player class that can handle player and draw player
+ */
 var client_player = function () {
     this.pos = {
         x: 300,
